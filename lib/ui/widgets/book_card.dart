fb13cbf6ec30a1209408fedefec256457401b42a
elxsson <soullcakezz@gmail.com>
2026-06-14 17:02:34 -0300

    feat: adiciona widgets reutilizaveis

---
+++ lib/ui/widgets/book_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_colors.dart';
+import '../../models/book.dart';
+import 'book_cover.dart';
+
+class BookCard extends StatelessWidget {
+  final Book book;
+  final VoidCallback? onTap;
+
+  const BookCard({
+    super.key,
+    required this.book,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 140,
+        margin: const EdgeInsets.only(right: 12),
+        decoration: BoxDecoration(
+          color: AppColors.secondary,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.08),
+              blurRadius: 8,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+              child: BookCover(
+                coverId: book.coverId,
+                width: 140,
+                height: 180,
+                size: 'M',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    book.title,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                      color: AppColors.primary,
+                    ),
+                  ),
+                  if (book.authors.isNotEmpty)
+                    Text(
+                      book.authors.first,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 11,
+                        color: AppColors.primary.withOpacity(0.7),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
